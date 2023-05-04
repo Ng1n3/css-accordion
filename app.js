2f13db32663f1b86cdb38d3b3eb5c4e31b5f982e
ng1n3 <code.with.muyiwa@gmail.com>
2023-05-04 12:24:01 +0100

    fixed accordion js

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 const icon = document.querySelector('.accordion__icon');
 const header = document.querySelector('.accordion__header');
+const accordion = document.querySelector('.accordion');
+
+accordion.addEventListener('click', (e) => {
+    const activePanel = e.target.closest('.accordion__panel');
+    if (!activePanel) return;
+    toggleAccordion(activePanel);
+});
+
+function toggleAccordion(panelToActivate) {
+    const activeButton = panelToActivate.querySelector('.accordion__icon');
+    const activePane = panelToActivate.querySelector('.accordion__content');
+    const activePanelIsOpened = activeButton.getAttribute('aria-expanded');
+
+    if(activePanelIsOpened === 'true') {
+        panelToActivate.querySelector('.accordion__icon').setAttribute('aria-expanded', false);
+
+        panelToActivate.querySelector('.accordion__content').setAttribute('aria-hidden', true);
+    }else  {
+        panelToActivate.querySelector('.accordion__icon').setAttribute('aria-expanded', true);
+
+        panelToActivate.querySelector('.accordion__content').setAttribute('aria-hidden', false);
+    }
+}
